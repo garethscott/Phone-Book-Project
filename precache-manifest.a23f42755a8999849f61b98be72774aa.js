e48bdbdf747ac3b31e2dc543623c5d09dfbfc8a8
garethscott <59648675+garethscott@users.noreply.github.com>
2020-07-29 09:22:33 +0100

    Updates

---
+++ precache-manifest.a23f42755a8999849f61b98be72774aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fba6e027071bd7acb0edb0ce35fb47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6519d9581aba8833b14e",
+    "url": "/static/css/main.1d353cd1.chunk.css"
+  },
+  {
+    "revision": "2d3629a5fc3fe6f6b2c1",
+    "url": "/static/js/2.ca8c6830.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca8c6830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6519d9581aba8833b14e",
+    "url": "/static/js/main.63a56f17.chunk.js"
+  },
+  {
+    "revision": "3b3cc7b88ecdec460469",
+    "url": "/static/js/runtime-main.0be0128f.js"
+  }
+]);
